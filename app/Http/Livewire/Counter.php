d8d66b13a207390029b4710280b165e0b1e79298
Mouhcine Slimani <muhcine.slimani@gmail.com>
2023-02-06 00:50:30 +0100

    updated

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -2,10 +2,43 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Counter extends Component
 {
+    public $name ;
+    public $isSet  ;
+    public $count  ;
+
+    public function mount()
+    {
+        //$this->name = $request->input('name');Request $request as arg
+        $this->name = "Slimani" ;
+        $this->isSet = false ;
+        $this->count = 0 ;
+    }
+
+    public function hydrate()
+    {
+        $this->name = "mouhcine";
+    }
+
+
+    public function updatedName()
+    {
+        $this->count++ ;
+    }
+
+    public function increment()
+    {
+        return $this->count ++ ;
+    }
+
+    public function decrement()
+    {
+        return $this->count -- ;
+    }
     public function render()
     {
         return view('livewire.counter');
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,10 @@
 <div>
-    {{-- Be like water. --}}
+    <input type="text" name="" wire:model.lazy="name" id="">
+    <input type="checkbox" name="" wire:model="isSet" id="">
+    <button wire:click="increment">increment</button>
+    <button wire:click="decrement">decrement</button>
+    {{-- @if ($isSet) --}}
+    {{ strtoupper($name) }}
+    {{-- @endif --}}
+    <h1>{{ $count }}</h1>
 </div>
